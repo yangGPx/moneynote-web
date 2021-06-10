82f5284866d6bfb3016f8b892c55108cb6a278bc
yangGPx <1299563700@qq.com>
2021-06-10 17:27:45 +0800

    deploy

---
+++ precache-manifest.51d475d1c2fcbfb746f0371f719f0587.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475f9a56bf26ae26ce4a",
+    "url": "/moneynote-web/css/Labels.b6999d01.css"
+  },
+  {
+    "revision": "eea7f87fcc6009dcba17",
+    "url": "/moneynote-web/css/LabelsEdit.ca092b0f.css"
+  },
+  {
+    "revision": "3e37621bef7644b8d36c",
+    "url": "/moneynote-web/css/app.bb623973.css"
+  },
+  {
+    "revision": "f11db6566a330c5fd019",
+    "url": "/moneynote-web/css/statistics.4eb217e3.css"
+  },
+  {
+    "revision": "0b631b66bf430a8194178b325bb55276",
+    "url": "/moneynote-web/index.html"
+  },
+  {
+    "revision": "475f9a56bf26ae26ce4a",
+    "url": "/moneynote-web/js/Labels.a9121f80.js"
+  },
+  {
+    "revision": "eea7f87fcc6009dcba17",
+    "url": "/moneynote-web/js/LabelsEdit.a79df73a.js"
+  },
+  {
+    "revision": "3e37621bef7644b8d36c",
+    "url": "/moneynote-web/js/app.a56d4d62.js"
+  },
+  {
+    "revision": "5384bfc5455e8daaa824",
+    "url": "/moneynote-web/js/chunk-vendors.11dda864.js"
+  },
+  {
+    "revision": "87daa7f2b6ba2a6bc6d5",
+    "url": "/moneynote-web/js/notFound.7ed0843b.js"
+  },
+  {
+    "revision": "f11db6566a330c5fd019",
+    "url": "/moneynote-web/js/statistics.93a0e721.js"
+  },
+  {
+    "revision": "66667cbd4f4fc0d10cf880a46a1a4150",
+    "url": "/moneynote-web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneynote-web/robots.txt"
+  }
+]);
